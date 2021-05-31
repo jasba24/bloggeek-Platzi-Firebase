06557576a126562d52e9a3415b0f6b3815952adb
jasba24 <johanbuitrago342@gmail.com>
2021-05-31 15:09:59 -0500

    feat: :fire: add google and facebook authentication

--- public/js/auth/autenticacion.js
+++ public/js/auth/autenticacion.js
@@ -1,34 +1,86 @@
 class Autenticacion {
-  autEmailPass (email, password) {
-    //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
-    //Materialize.toast(`Bienvenido ${result.user.displayName}`, 5000)
-    //$('.modal').modal('close')
-   
+	autEmailPass(email, password) {
+		firebase
+			.auth()
+			.signInWithEmailAndPassword(email, password)
+			.then(res => {
+				if (res.user.emailVerified) {
+					$("#avatar").attr("src", "imagenes/usuario_auth.png")
+					Materialize.toast(`Bienvenido ${res.user.displayName}`, 5000)
+				} else {
+					firebase.auth().signOut()
+					Materialize.toast(
+						`Por favor realiza la verificación de la cuenta`,
+						5000
+					)
+				}
+				$(".modal").modal("close")
+			})
+	}
+
+	crearCuentaEmailPass(email, password, nombres) {
+		firebase
+			.auth()
+			.createUserWithEmailAndPassword(email, password)
+			.then(res => {
+				res.user.updateProfile({
+					displayName: nombres,
+				})
+
+				const config = {
+					url: "http://localhost:3000/",
+				}
+
+				res.user.sendEmailVerification(config).catch(err => {
+					console.log(err)
+					Materialize.toast(err.message, 400)
+				})
+
+				firebase.auth().signOut()
+
+				Materialize.toast(
+					`Bienvenido ${nombres}, debes realizar el proceso de verificación`,
+					4000
+				)
+
+				$(".modal").modal("close")
+			})
+			.catch(err => {
+				console.log(err)
+				Materialize.toast(err.message, 400)
+			})
+	}
+
+	authCuentaGoogle() {
+		const provider = new firebase.auth.GoogleAuthProvider()
+		firebase
+			.auth()
+			.signInWithPopup(provider)
+			.then(res => {
+				$("#avatar").attr("src", res.user.photoURL)
+				$(".modal").modal("close")
+				Materialize.toast(`Bienvenido ${res.user.displayName} !! `, 4000)
+			})
+			.catch(err => {
+				console.log(err)
+				Materialize.toast(`Error al autenticarse con google ${err}`, 4000)
+			})
+	}
+
+	authCuentaFacebook () {
+    const provider = new firebase.auth.FacebookAuthProvider();
+    firebase.auth().signInWithPopup(provider).then(result => {
+      $('#avatar').attr('src', result.user.photoURL)
+      $('.modal').modal('close')
+      Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
+    })
+    .catch(error =>{
+      console.error(error)
+      Materialize.toast(`Error al autenticarse con facebook: ${error} `, 4000)
+    })
   }
 
-  crearCuentaEmailPass (email, password, nombres) {
-    /*Materialize.toast(
-      `Bienvenido ${nombres}, debes realizar el proceso de verificación`,
-      4000
-    )
-
-    $('.modal').modal('close')*/
-    
-  }
-
-  authCuentaGoogle () {
-    //$('#avatar').attr('src', result.user.photoURL)
-    //$('.modal').modal('close')
-    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
-  }
-
-  authCuentaFacebook () {
-    //$('#avatar').attr('src', result.user.photoURL)
-    //$('.modal').modal('close')
-    //Materialize.toast(`Bienvenido ${result.user.displayName} !! `, 4000)
-  }
-
-  authTwitter () {
-    // TODO: Crear auth con twitter
-  }
+	authTwitter() {
+		// TODO: Crear auth con twitter
+	}
 }
--- public/js/general.js
+++ public/js/general.js
@@ -1,51 +1,57 @@
 $(() => {
-  $('.tooltipped').tooltip({ delay: 50 })
-  $('.modal').modal()
+	$(".tooltipped").tooltip({ delay: 50 })
+	$(".modal").modal()
 
-  // TODO: Adicionar el service worker
+	// TODO: Adicionar el service worker
 
-  // Init Firebase nuevamente
-  firebase.initializeApp(config);
+	// Initialize Firebase
+	firebase.initializeApp(firebaseConfig)
+	firebase.analytics()
 
-  // TODO: Registrar LLave publica de messaging
+	// TODO: Registrar LLave publica de messaging
 
-  // TODO: Solicitar permisos para las notificaciones
+	// TODO: Solicitar permisos para las notificaciones
 
-  // TODO: Recibir las notificaciones cuando el usuario esta foreground
+	// TODO: Recibir las notificaciones cuando el usuario esta foreground
 
-  // TODO: Recibir las notificaciones cuando el usuario esta background
+	// TODO: Recibir las notificaciones cuando el usuario esta background
 
-  // TODO: Listening real time
+	// TODO: Listening real time
 
-  // TODO: Firebase observador del cambio de estado
-  //$('#btnInicioSesion').text('Salir')
-  //$('#avatar').attr('src', user.photoURL)
-  //$('#avatar').attr('src', 'imagenes/usuario_auth.png')
-  //$('#btnInicioSesion').text('Iniciar Sesión')
-  //$('#avatar').attr('src', 'imagenes/usuario.png')
+	// TODO: Firebase observador del cambio de estado
+	//$('#btnInicioSesion').text('Salir')
+	//$('#avatar').attr('src', user.photoURL)
+	//$('#avatar').attr('src', 'imagenes/usuario_auth.png')
+	//$('#btnInicioSesion').text('Iniciar Sesión')
+	//$('#avatar').attr('src', 'imagenes/usuario.png')
 
-  // TODO: Evento boton inicio sesion
-  $('#btnInicioSesion').click(() => {
-    //$('#avatar').attr('src', 'imagenes/usuario.png')
-    // Materialize.toast(`Error al realizar SignOut => ${error}`, 4000)
-    
+	// TODO: Evento boton inicio sesion
+	$("#btnInicioSesion").click(() => {
+		//$('#avatar').attr('src', 'imagenes/usuario.png')
+		// Materialize.toast(`Error al realizar SignOut => ${error}`, 4000)
 
-    $('#emailSesion').val('')
-    $('#passwordSesion').val('')
-    $('#modalSesion').modal('open')
-  })
+		$("#emailSesion").val("")
+		$("#passwordSesion").val("")
+		$("#modalSesion").modal("open")
+	})
 
-  $('#avatar').click(() => {
-    //$('#avatar').attr('src', 'imagenes/usuario.png')
-    //Materialize.toast(`SignOut correcto`, 4000)
-  })
+	$("#avatar").click(() => {
+		firebase
+			.auth()
+			.signOut()
+			.then(() => {
+				$("#avatar").attr("src", "imagenes/usuario.png")
+				Materialize.toast(`SignOut correcto`, 4000)
+			})
+			.catch(err => Materialize.toast(`Error al realizar SignOut ${err}`, 4000))
+	})
 
-  $('#btnTodoPost').click(() => {
-    $('#tituloPost').text('Posts de la Comunidad')   
-  })
+	$("#btnTodoPost").click(() => {
+		$("#tituloPost").text("Posts de la Comunidad")
+	})
 
-  $('#btnMisPost').click(() => {
-    //$('#tituloPost').text('Mis Posts')
-    //Materialize.toast(`Debes estar autenticado para ver tus posts`, 4000)    
-  })
+	$("#btnMisPost").click(() => {
+		//$('#tituloPost').text('Mis Posts')
+		//Materialize.toast(`Debes estar autenticado para ver tus posts`, 4000)
+	})
 })
